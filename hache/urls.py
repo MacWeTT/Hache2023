64374ecef0207c8c8dc1bfb9968555c4b3888afb
Manas Bajpai <manasbajpai18@gmail.com>
2023-04-11 23:36:55 +0530

    added custom admin url

--- hache/urls.py
+++ hache/urls.py
@@ -5,8 +5,8 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('quizapp.urls')),
+    path('notforyou23ok/', admin.site.urls),
+    path('', include('quizapp.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
