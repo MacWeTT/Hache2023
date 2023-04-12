db13d97c39f68016aafa8b7443b5418df55a2775
Manas Bajpai <manasbajpai18@gmail.com>
2023-04-12 15:35:48 +0530

    ugh

--- quizapp/verified_csv.py
+++ quizapp/verified_csv.py
@@ -6,7 +6,7 @@ from hache.settings import BASE_DIR
 
 def run():
     pathh = os.path.join(BASE_DIR, 'emails.csv')
-    dfList = pandas.read_csv(pathh)['emails'].tolist()
+    dfList = pandas.read_csv(pathh)['EMAILS'].tolist()
     all_emails = [*set(dfList)]
     bulk_list = list()
     for i in range(len(all_emails)):
